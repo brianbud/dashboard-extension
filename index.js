ee791055e8b3265a45ef9a4f33d21d8a7947900e
Brian Bud <hello@brianbud.com>
2024-01-21 10:42:44 -0500

    display author of the picture

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const authorEl = document.querySelector('#author');
+
 fetch(
   'https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=CqZg12U9DultJO-WEbv2bQ5lnAuuGmvUe4W16PXykvE'
 )
   .then((res) => res.json())
   .then((data) => {
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    authorEl.innerHTML = `<h3>by: ${data.user.name}</h3>`;
   });
