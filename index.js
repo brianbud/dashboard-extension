a82b2c792c135650bef7558fd5ec610033ca1b6f
Brian Bud <hello@brianbud.com>
2024-01-21 11:33:31 -0500

    convert to async await func and add catch

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const authorEl = document.querySelector('#author');
 
-fetch(
-  'https://api.unsplash.com/photos/random?orientation=landscape&query=nature&clientt_id=CqZg12U9DultJO-WEbv2bQ5lnAuuGmvUe4W16PXykvE'
-)
-  .then((res) => res.json())
-  .then((data) => {
+async function getBgImage() {
+  try {
+    const res = await fetch(
+      'https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=CqZg12U9DultJO-WEbv2bQ5lnAuuGmvUe4W16PXykvE'
+    );
+    const data = await res.json();
+
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
     authorEl.innerHTML = `<h3>by: ${data.user.name}</h3>`;
-  })
-  .catch((err) => {
+  } catch (err) {
     console.log(err);
     document.body.style.backgroundImage = `url("https://source.unsplash.com/random?orientation=landscape&query=nature")`;
-  });
+  }
+}
+
+getBgImage();
+// fetch(
+//   'https://api.unsplash.com/photos/random?orientation=landscape&query=nature&clientt_id=CqZg12U9DultJO-WEbv2bQ5lnAuuGmvUe4W16PXykvE'
+// )
+//   .then((res) => res.json())
+//   .then((data) => {
+//     document.body.style.backgroundImage = `url(${data.urls.regular})`;
+//     authorEl.innerHTML = `<h3>by: ${data.user.name}</h3>`;
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     document.body.style.backgroundImage = `url("https://source.unsplash.com/random?orientation=landscape&query=nature")`;
+//   });
