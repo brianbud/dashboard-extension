216e95436c75a56835859257af276b9e8c2a3934
Brian Bud <hello@brianbud.com>
2024-01-22 20:32:38 -0500

    convert currentDate to string

--- index.js
+++ index.js
@@ -16,15 +16,25 @@ async function getBgImage() {
 }
 
 getBgImage();
-// fetch(
-//   'https://api.unsplash.com/photos/random?orientation=landscape&query=nature&clientt_id=CqZg12U9DultJO-WEbv2bQ5lnAuuGmvUe4W16PXykvE'
-// )
-//   .then((res) => res.json())
-//   .then((data) => {
-//     document.body.style.backgroundImage = `url(${data.urls.regular})`;
-//     authorEl.innerHTML = `<h3>by: ${data.user.name}</h3>`;
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//     document.body.style.backgroundImage = `url("https://source.unsplash.com/random?orientation=landscape&query=nature")`;
-//   });
+
+async function getStockPrice() {
+  try {
+    const res = await fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SHOP.TRT&outputsize=full&apikey=1R886Z5JGWXHTFBY`
+    );
+    const data = await res.json();
+
+    const date = new Date();
+
+    let day = date.getDate().toString().padStart(2, '0');
+    let month = (date.getMonth() + 1).toString().padStart(2, '0');
+    let year = date.getFullYear();
+    let currentDate = `${year}-${month}-${day}`;
+    console.log(data);
+    console.log(data['Time Series (Daily)'][currentDate]);
+  } catch {
+    console.log(err);
+  }
+}
+
+getStockPrice();
